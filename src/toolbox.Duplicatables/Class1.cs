01c14a1fdd14328a71f3de3568ee65bc8113c719
Marcel Nika <mnka.110702@gmail.com>
2026-04-04 15:54:17 +0200

    create prototype design pattern library

---
+++ src/toolbox.Duplicatables/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace toolbox.Duplicatables;
+/// <summary>
+/// Defines an interface for cloning objects. This is a key component of the Prototype design pattern,
+/// allowing objects to create copies of themselves without depending on their concrete classes.
+/// </summary>
+/// <typeparam name="TDuplicatable"></typeparam>
+public interface IDuplicatable <TDuplicatable> {
+    public TDuplicatable Copy();
+}
